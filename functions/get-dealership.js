c7fe18e7b006d7737c9ff23ab333e8ba3f14f83e
Kay Tahk <kayleightahk@gmail.com>
2024-02-06 23:59:00 -0500

    added CLI endpoints

--- functions/get-dealership.js
+++ functions/get-dealership.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const Cloudant = require('@cloudant/cloudant');
+
 // Initialize Cloudant connection with IAM authentication
 async function dbCloudantConnect() {
     try {
         const cloudant = Cloudant({
-            plugins: { iamauth: { iamApiKey: "UIJINzQoPjwJgJrakLCmvD2nhXJitpVpjnp55_-kCzkO" } }, // Replace with your IAM API key
-            url: "https://c59f03a4-aa53-4c33-812f-fe02dbcc4fc4-bluemix.cloudantnosqldb.appdomain.cloud", // Replace with your Cloudant URL
+            plugins: { iamauth: { iamApiKey: 'UIJINzQoPjwJgJrakLCmvD2nhXJitpVpjnp55_-kCzkO' } }, // Replace with your IAM API key
+            url: 'https://c59f03a4-aa53-4c33-812f-fe02dbcc4fc4-bluemix.cloudantnosqldb.appdomain.cloud', // Replace with your Cloudant URL
         });
+
         const db = cloudant.use('dealerships');
         console.info('Connect success! Connected to DB');
         return db;
@@ -17,14 +19,19 @@ async function dbCloudantConnect() {
         throw err;
     }
 }
+
 let db;
+
 (async () => {
     db = await dbCloudantConnect();
 })();
+
 app.use(express.json());
+
 // Define a route to get all dealerships with optional state and ID filters
 app.get('/dealerships/get', (req, res) => {
     const { state, id } = req.query;
+
     // Create a selector object based on query parameters
     const selector = {};
     if (state) {
@@ -34,10 +41,12 @@ app.get('/dealerships/get', (req, res) => {
     if (id) {
         selector.id = parseInt(id); // Filter by "id" with a value of 1
     }
+
     const queryOptions = {
         selector,
         limit: 10, // Limit the number of documents returned to 10
     };
+
     db.find(queryOptions, (err, body) => {
         if (err) {
             console.error('Error fetching dealerships:', err);
@@ -48,6 +57,7 @@ app.get('/dealerships/get', (req, res) => {
         }
     });
 });
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
